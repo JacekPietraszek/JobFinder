0bfc866e638211f231824221fdeaa0d610a4b257
Jacek Pietraszek <jacekpietraszek01@gmail.com>
2023-11-25 08:54:10 +0100

    Creates JobOfferDtom Mapperm Service

--- src/main/java/com/example/apicrud/JobOfferController.java
+++ src/main/java/com/example/apicrud/JobOfferController.java
@@ -1,21 +1,23 @@
 package com.example.apicrud;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 public class JobOfferController {
-    private final JobOfferRepository jobOfferRepository;
+    private final JobOfferService jobOfferService;
 
-    public JobOfferController(JobOfferRepository jobOfferRepository) {
-        this.jobOfferRepository = jobOfferRepository;
+    public JobOfferController(JobOfferService jobOfferService) {
+        this.jobOfferService = jobOfferService;
     }
 
     @GetMapping("/offers/{id}")
-    Optional<JobOffer> getOfferById(@PathVariable Long id) {
-        return jobOfferRepository.findById(id);
+    ResponseEntity<JobOfferDto> getOfferById(@PathVariable Long id) {
+        return jobOfferService.getOfferById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/com/example/apicrud/JobOfferService.java
@@ -0,0 +1,20 @@
+package com.example.apicrud;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobOfferService {
+    private JobOfferRepository jobOfferRepository;
+    private JobOfferDtoMapper jobOfferDtoMapper;
+
+    public JobOfferService(JobOfferRepository jobOfferRepository, JobOfferDtoMapper jobOfferDtoMapper) {
+        this.jobOfferRepository = jobOfferRepository;
+        this.jobOfferDtoMapper = jobOfferDtoMapper;
+    }
+
+    Optional<JobOfferDto> getOfferById(Long id) {
+        return jobOfferRepository.findById(id).map(jobOfferDtoMapper::map);
+    }
+}
